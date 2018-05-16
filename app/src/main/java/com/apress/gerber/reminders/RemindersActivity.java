b342c52ffa8848812a85bfc8f27ef10c32a18c64
git <rocky10000@126.com>
2018-05-16 16:21:52 +0800

    Adds SQLite database persistence for reminders and a new color for important reminders

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -1,23 +1,48 @@
 package com.apress.gerber.reminders;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class RemindersActivity extends AppCompatActivity {
     private ListView mListView;
+    private RemindersDbAdapter mDbAdapter;
+    private RemindersSimpleCursordapter mCursordapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
         mListView=findViewById(R.id.reminds_list_view);
+        mListView.setDivider(null);
+        mDbAdapter=new RemindersDbAdapter(this);
+        mDbAdapter.open();
+
+        Cursor cursor=mDbAdapter.fetchAllReminders();
+
+        //from columns defined in the db
+        String[] from=new String[]{RemindersDbAdapter.COL_CONTENT};
+        //to the ids of views in the layout
+        int[] to=new int[]{R.id.row_text};
+
+        mCursordapter=new RemindersSimpleCursordapter(this
+        ,R.layout.reminders_row
+        ,cursor
+        ,from
+        ,to
+        ,0);
+
         //The arrayAdapter is the controller in our model-view-controller relationship.(controller)
-        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<>(this,R.layout.reminders_row,R.id.row_text,new String[]{"First record","second record","third record"});
-        mListView.setAdapter(arrayAdapter);
+        //ArrayAdapter<String> arrayAdapter=new ArrayAdapter<>(this,R.layout.reminders_row,R.id.row_text,new String[]{"First record","second record","third record"});
+        //mListView.setAdapter(arrayAdapter);
+
+        //the cursorAdapter(controller) is now updating the listView(view) with data from the db (model)
+        mListView.setAdapter(mCursordapter);
     }
     //Reminder of the class listing omitted for brevity
 
