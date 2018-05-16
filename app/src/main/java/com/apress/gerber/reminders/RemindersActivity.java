8ff2eabeb0a2eaa84e57eb16f6e809d1b48a0813
git <rocky10000@126.com>
2018-05-16 12:15:54 +0800

    Adds new reminder and exit menu options

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -1,7 +1,10 @@
 package com.apress.gerber.reminders;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -10,11 +13,33 @@ public class RemindersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_reminders);
         mListView=findViewById(R.id.reminds_list_view);
         //The arrayAdapter is the controller in our model-view-controller relationship.(controller)
         ArrayAdapter<String> arrayAdapter=new ArrayAdapter<>(this,R.layout.reminders_row,R.id.row_text,new String[]{"First record","second record","third record"});
         mListView.setAdapter(arrayAdapter);
     }
     //Reminder of the class listing omitted for brevity
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_reminders,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_new:
+                //create new Reminder
+                Log.d(getLocalClassName(), "Create new Reminder");
+                break;
+            case R.id.action_exit:
+                finish();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
 }
